1ee7df38847ea13cc69ef83ec7a5e3bba2031b53
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-03-31 11:35:17 +0600

    navbar added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Knowledge Cafe</h2>
+            <div className='header-right'>
+                <a href="">Home</a>
+                <a href="">Portfolio</a>
+                <a href="">subscribe</a>
+                <img src="../../../img/images.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;

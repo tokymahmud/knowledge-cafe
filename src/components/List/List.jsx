db684cb0e05d33e5703bafec82b26b2a1791af54
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-04-01 01:39:23 +0600

    handled time spent

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,9 +4,13 @@ import './List.css'
 const List = (props) => {
     const {list} =props;
     console.log(list);
+    let total =0;
+    for (const time of list ){
+        total = total + time.read_time;
+    }
     return (
         <div>
-             <h4 className='listOne'>Spent time on read : {list.length}</h4>
+             <h4 className='listOne'>Spent time on read : {total}</h4>
                 <h4>Bookmarked Blogs : </h4>
         </div>
     );

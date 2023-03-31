6f12e090568049360eb30a933c6cefdd912f2b53
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-03-31 15:45:35 +0600

    body work started

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Body.css"
+
+const Body = () => {
+    return (
+        <div className='body-container'>
+            <div className="blog-container">
+                <h3>BLog page</h3>
+
+            </div>
+            <div className="list-container">
+                <h4>Spent time on read : </h4>
+                <h4>Bookmarked Blogs : </h4>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Body;

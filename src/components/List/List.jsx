de7f3444d01cd0a9959ed1250a13323cc39e0d4f
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-04-01 14:31:32 +0600

    bookmark added

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,14 +8,16 @@ const List = (props) => {
         total = total + time.read_time;
     }
     let totalBookmark =0;
+    let bookMname ='';
     for (const name of listB ){
         totalBookmark = totalBookmark + 1;
+        bookMname =bookMname +name.blogTitle
     }
     return (
         <div>
              <h4 className='listOne'>Spent time on read : {total} min</h4>
                 <h4 className='listOne'>Bookmarked Blogs :{totalBookmark}</h4>
-                <h3 className='BookmarkPage'>{list.blogTitle}</h3>
+                <h3 className='BookmarkPage'>{bookMname}</h3>
         </div>
     );
 };
